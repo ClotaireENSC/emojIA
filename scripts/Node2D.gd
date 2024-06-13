2a60940ff924a7c3eb18b97e9ee6caaecdfe9a8c
Clotaire <cmardon001@ensc.fr>
2024-06-13 18:38:00 +0200

    .

--- scripts/Node2D.gd
+++ scripts/Node2D.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
+@onready var node_2d = $"."
 var screenshots_folder = "res://assets/screenshots/"
 var images_folder = "res://assets/emojis"
+var scenes_folder = "res://assets/pop_scenes/"
 var image_files = []
-@onready var node_2d = $"."
 var viewport_size = Vector2(1000, 1000)
+var screenshot_path : String
+var score : float
+
+@export var id : int
 
 func _ready():
 	get_files_from_folder(images_folder)
 	
 	get_viewport().set_size(viewport_size)
 	
-	for l in range(50):
-		await generate_image(l)
+	await generate_image(id)
+	
+	get_score()
 
 func get_files_from_folder(path):
 	var dir = DirAccess.open(path)
@@ -49,9 +55,45 @@ func generate_image(k):
 	
 	await get_tree().process_frame
 	await get_tree().process_frame
-	var screenshot_path = screenshots_folder + "screenshot_%s.png" % k
+	screenshot_path = screenshots_folder + "screenshot_%s.png" % k
 	var screenshot = get_viewport().get_texture().get_image()
 	screenshot.save_png(screenshot_path)
-	print("Screenshot saved to: " + screenshot_path)
 	
-	emojis.queue_free()
+	#save_scene("pop_scene_%s.tscn" % k)
+
+func get_score():
+	var image = Image.new()
+	image.load(screenshot_path)
+	
+	var tmp_score = 0
+	
+	var photo = Image.new()
+	photo.load("res://assets/photos/moi2.jpg")
+	photo.resize(1000,1000)
+	
+	for x in range(image.get_width()):
+		for y in range(image.get_height()):
+			var color = image.get_pixel(x, y)
+			var color2 = photo.get_pixel(x, y)
+			tmp_score +=  pixel_distance(color, color2)
+	
+	score = tmp_score
+
+func pixel_distance(pixel1: Color, pixel2: Color) -> float:
+	var r_diff = pixel2.r - pixel1.r
+	var g_diff = pixel2.g - pixel1.g
+	var b_diff = pixel2.b - pixel1.b
+
+	var distance = sqrt(r_diff * r_diff + g_diff * g_diff + b_diff * b_diff)
+
+	return distance
+
+#func save_scene(filename):
+	#var save = PackedScene.new()
+	#for c in self.get_children():
+		#c.set_owner(self)
+		#for a in c.get_children():
+			#a.set_owner(self)
+	#save.pack(self);
+#
+	#ResourceSaver.save(save, "res://assets/pop_scenes/"  + filename);
